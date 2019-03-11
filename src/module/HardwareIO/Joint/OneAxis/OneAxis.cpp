7c508753304730d5bf616337aed422da8dedd3d7
Taylor <c3206230@uon.edu.au>
2019-03-11 20:36:18 +1100

    Joint math

--- src/module/HardwareIO/Joint/OneAxis/OneAxis.cpp
+++ src/module/HardwareIO/Joint/OneAxis/OneAxis.cpp
@@ -6,6 +6,15 @@ namespace HardwareIO {
     namespace joint {
 
         OneAxis::OneAxis() {}
+
+        // Some sort of initial position, 0
+        // Some sort of initial length for all of the muscles
+
+        // Input position
+
+        double L_1 = 2 * l_01 + l_02 - L_2;
+        double L_2 = (2 * M_PI() * r_1 * / 360);
+
     }  // namespace joint
 }  // namespace HardwareIO
 }  // namespace module
--- src/module/HardwareIO/Joint/TwoAxis/TwoAxis.cpp
+++ src/module/HardwareIO/Joint/TwoAxis/TwoAxis.cpp
@@ -6,6 +6,12 @@ namespace HardwareIO {
     namespace joint {
 
         TwoAxis::TwoAxis() {}
+
+        double L_1 = 2 * l_01 + l_02 + l_04 - L_2 - L_4;
+        double L_2 = (2 * M_PI() * r_1 * / 360) + l_02 + l_03 - L_3;
+        double L_3 = (2 * M_PI() * r_1 * / 360) + (2 * M_PI() * r_2 * / 360);
+        double L_4 = (2 * M_PI() * r_2 * / 360) + l_04 + l_03 - L_3;
+
     }  // namespace joint
 }  // namespace HardwareIO
 }  // namespace module
