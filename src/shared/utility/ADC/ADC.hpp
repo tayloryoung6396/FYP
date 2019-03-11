06e0afbadd24b35e4360882c1b47708523a8c195
Taylor <c3206230@uon.edu.au>
2019-03-11 20:36:13 +1100

    ADC class

---
+++ src/shared/utility/ADC/ADC.hpp
@@ -0,0 +1,14 @@
+#ifndef MODULE_ADC_HPP
+#define MODULE_ADC_HPP
+
+namespace shared {
+namespace utility {
+    class ADC {
+    public:
+        ADC();
+    };
+
+}  // namespace utility
+}  // namespace shared
+
+#endif  // MODULE_ADC_HPP
