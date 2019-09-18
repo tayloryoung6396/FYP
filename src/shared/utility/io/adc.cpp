c1729080fb0ba986d8feb3a0d4510b692e51b85e
Unknown <c3206230@uon.edu.au>
2019-09-18 13:50:53 +1000

    Clean file

--- src/shared/utility/io/adc.cpp
+++ src/shared/utility/io/adc.cpp
@@ -7,10 +7,10 @@
 extern "C" {
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* AdcHandle) {
     if (AdcHandle == &hadc1) {
-        // TODO Some sort of critical section should be used for these
+        // TODO Some sort of critical section should be used for these maybe?
     }
     else if (AdcHandle == &hadc3) {
-        // TODO Some sort of critical section should be used for these
+        // TODO Some sort of critical section should be used for these maybe?
     }
     else {
         utility::io::debug.out("I got an interupt but i dont know why\n");
@@ -27,7 +27,6 @@ void MX_ADC1_Init(void) {
     ADC_ChannelConfTypeDef sConfig = {0};
 
     // Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
-
     hadc1.Instance                   = ADC1;
     hadc1.Init.ClockPrescaler        = ADC_CLOCK_SYNC_PCLK_DIV4;
     hadc1.Init.Resolution            = ADC_RESOLUTION_12B;
@@ -43,42 +42,41 @@ void MX_ADC1_Init(void) {
     if (HAL_ADC_Init(&hadc1) != HAL_OK) {
         Error_Handler();
     }
-    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
 
+    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
     sConfig.Channel      = ADC_CHANNEL_0;
     sConfig.Rank         = ADC_REGULAR_RANK_1;
     sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
     if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK) {
         Error_Handler();
     }
-    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
 
+    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
     sConfig.Channel = ADC_CHANNEL_1;
     sConfig.Rank    = ADC_REGULAR_RANK_2;
     if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK) {
         Error_Handler();
     }
-    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
 
+    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
     sConfig.Channel = ADC_CHANNEL_4;
     sConfig.Rank    = ADC_REGULAR_RANK_3;
     if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK) {
         Error_Handler();
     }
-    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
 
+    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
     sConfig.Channel = ADC_CHANNEL_8;
     sConfig.Rank    = ADC_REGULAR_RANK_4;
     if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK) {
         Error_Handler();
     }
 }
-/* ADC3 init function */
+// ADC3 init function
 void MX_ADC3_Init(void) {
     ADC_ChannelConfTypeDef sConfig = {0};
 
     // Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
-
     hadc3.Instance                   = ADC3;
     hadc3.Init.ClockPrescaler        = ADC_CLOCK_SYNC_PCLK_DIV4;
     hadc3.Init.Resolution            = ADC_RESOLUTION_12B;
@@ -94,37 +92,37 @@ void MX_ADC3_Init(void) {
     if (HAL_ADC_Init(&hadc3) != HAL_OK) {
         Error_Handler();
     }
-    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
 
+    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
     sConfig.Channel      = ADC_CHANNEL_4;
     sConfig.Rank         = ADC_REGULAR_RANK_1;
     sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
     if (HAL_ADC_ConfigChannel(&hadc3, &sConfig) != HAL_OK) {
         Error_Handler();
     }
-    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
 
+    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
     sConfig.Channel = ADC_CHANNEL_5;
     sConfig.Rank    = ADC_REGULAR_RANK_2;
     if (HAL_ADC_ConfigChannel(&hadc3, &sConfig) != HAL_OK) {
         Error_Handler();
     }
-    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
 
+    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
     sConfig.Channel = ADC_CHANNEL_12;
     sConfig.Rank    = ADC_REGULAR_RANK_3;
     if (HAL_ADC_ConfigChannel(&hadc3, &sConfig) != HAL_OK) {
         Error_Handler();
     }
-    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
 
+    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
     sConfig.Channel = ADC_CHANNEL_13;
     sConfig.Rank    = ADC_REGULAR_RANK_4;
     if (HAL_ADC_ConfigChannel(&hadc3, &sConfig) != HAL_OK) {
         Error_Handler();
     }
-    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
 
+    // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
     sConfig.Channel = ADC_CHANNEL_11;
     sConfig.Rank    = ADC_REGULAR_RANK_5;
     if (HAL_ADC_ConfigChannel(&hadc3, &sConfig) != HAL_OK) {
@@ -138,26 +136,26 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle) {
     if (adcHandle->Instance == ADC1) {
 
 
-        /* ADC1 clock enable */
+        // ADC1 clock enable
         __HAL_RCC_ADC1_CLK_ENABLE();
 
         __HAL_RCC_GPIOA_CLK_ENABLE();
         __HAL_RCC_GPIOC_CLK_ENABLE();
         __HAL_RCC_GPIOB_CLK_ENABLE();
-        /**ADC1 GPIO Configuration
-        PA0-WKUP     ------> ADC1_IN0
-        PA1     ------> ADC1_IN1
-        PA2     ------> ADC1_IN2
-        PA3     ------> ADC1_IN3
-        PA4     ------> ADC1_IN4
-        PA5     ------> ADC1_IN5
-        PA6     ------> ADC1_IN6
-        PA7     ------> ADC1_IN7
-        PC4     ------> ADC1_IN14
-        PC5     ------> ADC1_IN15
-        PB0     ------> ADC1_IN8
-        PB1     ------> ADC1_IN9
-        */
+        // ADC1 GPIO Configuration
+        // PA0-WKUP     ------> ADC1_IN0
+        // PA1     ------> ADC1_IN1
+        // PA2     ------> ADC1_IN2
+        // PA3     ------> ADC1_IN3
+        // PA4     ------> ADC1_IN4
+        // PA5     ------> ADC1_IN5
+        // PA6     ------> ADC1_IN6
+        // PA7     ------> ADC1_IN7
+        // PC4     ------> ADC1_IN14
+        // PC5     ------> ADC1_IN15
+        // PB0     ------> ADC1_IN8
+        // PB1     ------> ADC1_IN9
+
         GPIO_InitStruct.Pin =
             GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7;
         GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
@@ -174,8 +172,8 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle) {
         GPIO_InitStruct.Pull = GPIO_NOPULL;
         HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-        /* ADC1 DMA Init */
-        /* ADC1 Init */
+        // ADC1 DMA Init
+        // ADC1 Init
         hdma_adc1.Instance                 = DMA2_Stream4;
         hdma_adc1.Init.Channel             = DMA_CHANNEL_0;
         hdma_adc1.Init.Direction           = DMA_PERIPH_TO_MEMORY;
@@ -192,34 +190,34 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle) {
 
         __HAL_LINKDMA(adcHandle, DMA_Handle, hdma_adc1);
 
-        /* ADC1 interrupt Init */
+        // ADC1 interrupt Init
         HAL_NVIC_SetPriority(ADC_IRQn, 0, 0);
         HAL_NVIC_EnableIRQ(ADC_IRQn);
     }
     else if (adcHandle->Instance == ADC3) {
 
 
-        /* ADC3 clock enable */
+        // ADC3 clock enable
         __HAL_RCC_ADC3_CLK_ENABLE();
 
         __HAL_RCC_GPIOF_CLK_ENABLE();
         __HAL_RCC_GPIOC_CLK_ENABLE();
         __HAL_RCC_GPIOA_CLK_ENABLE();
-        /**ADC3 GPIO Configuration
-        PF3     ------> ADC3_IN9
-        PF4     ------> ADC3_IN14
-        PF5     ------> ADC3_IN15
-        PF6     ------> ADC3_IN4
-        PF7     ------> ADC3_IN5
-        PF8     ------> ADC3_IN6
-        PF9     ------> ADC3_IN7
-        PF10     ------> ADC3_IN8
-        PC0     ------> ADC3_IN10
-        PC1     ------> ADC3_IN11
-        PC2     ------> ADC3_IN12
-        PC3     ------> ADC3_IN13
-        PA3     ------> ADC3_IN3
-        */
+        // ADC3 GPIO Configuration
+        // PF3     ------> ADC3_IN9
+        // PF4     ------> ADC3_IN14
+        // PF5     ------> ADC3_IN15
+        // PF6     ------> ADC3_IN4
+        // PF7     ------> ADC3_IN5
+        // PF8     ------> ADC3_IN6
+        // PF9     ------> ADC3_IN7
+        // PF10     ------> ADC3_IN8
+        // PC0     ------> ADC3_IN10
+        // PC1     ------> ADC3_IN11
+        // PC2     ------> ADC3_IN12
+        // PC3     ------> ADC3_IN13
+        // PA3     ------> ADC3_IN3
+
         GPIO_InitStruct.Pin =
             GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10;
         GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
@@ -236,8 +234,8 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle) {
         GPIO_InitStruct.Pull = GPIO_NOPULL;
         HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-        /* ADC3 DMA Init */
-        /* ADC3 Init */
+        // ADC3 DMA Init
+        // ADC3 Init
         hdma_adc3.Instance                 = DMA2_Stream1;
         hdma_adc3.Init.Channel             = DMA_CHANNEL_2;
         hdma_adc3.Init.Direction           = DMA_PERIPH_TO_MEMORY;
@@ -254,7 +252,7 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle) {
 
         __HAL_LINKDMA(adcHandle, DMA_Handle, hdma_adc3);
 
-        /* ADC3 interrupt Init */
+        // ADC3 interrupt Init
         HAL_NVIC_SetPriority(ADC_IRQn, 0, 0);
         HAL_NVIC_EnableIRQ(ADC_IRQn);
     }
@@ -265,23 +263,9 @@ void HAL_ADC_MspDeInit(ADC_HandleTypeDef* adcHandle) {
     if (adcHandle->Instance == ADC1) {
 
 
-        /* Peripheral clock disable */
+        // Peripheral clock disable
         __HAL_RCC_ADC1_CLK_DISABLE();
 
-        /**ADC1 GPIO Configuration
-        PA0-WKUP     ------> ADC1_IN0
-        PA1     ------> ADC1_IN1
-        PA2     ------> ADC1_IN2
-        PA3     ------> ADC1_IN3
-        PA4     ------> ADC1_IN4
-        PA5     ------> ADC1_IN5
-        PA6     ------> ADC1_IN6
-        PA7     ------> ADC1_IN7
-        PC4     ------> ADC1_IN14
-        PC5     ------> ADC1_IN15
-        PB0     ------> ADC1_IN8
-        PB1     ------> ADC1_IN9
-        */
         HAL_GPIO_DeInit(
             GPIOA,
             GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7);
@@ -290,38 +274,22 @@ void HAL_ADC_MspDeInit(ADC_HandleTypeDef* adcHandle) {
 
         HAL_GPIO_DeInit(GPIOB, GPIO_PIN_0 | GPIO_PIN_1);
 
-        /* ADC1 DMA DeInit */
+        // ADC1 DMA DeInit
         HAL_DMA_DeInit(adcHandle->DMA_Handle);
 
-        /* ADC1 interrupt Deinit */
+        // ADC1 interrupt Deinit
 
-        /**
-         * Uncomment the line below to disable the "ADC_IRQn" interrupt
-         * Be aware, disabling shared interrupt may affect other IPs
-         */
-        /* HAL_NVIC_DisableIRQ(ADC_IRQn); */
+        // * Uncomment the line below to disable the "ADC_IRQn" interrupt
+        // * Be aware, disabling shared interrupt may affect other IPs
+
+        // HAL_NVIC_DisableIRQ(ADC_IRQn);
     }
     else if (adcHandle->Instance == ADC3) {
 
 
-        /* Peripheral clock disable */
+        // Peripheral clock disable
         __HAL_RCC_ADC3_CLK_DISABLE();
 
-        /**ADC3 GPIO Configuration
-        PF3     ------> ADC3_IN9
-        PF4     ------> ADC3_IN14
-        PF5     ------> ADC3_IN15
-        PF6     ------> ADC3_IN4
-        PF7     ------> ADC3_IN5
-        PF8     ------> ADC3_IN6
-        PF9     ------> ADC3_IN7
-        PF10     ------> ADC3_IN8
-        PC0     ------> ADC3_IN10
-        PC1     ------> ADC3_IN11
-        PC2     ------> ADC3_IN12
-        PC3     ------> ADC3_IN13
-        PA3     ------> ADC3_IN3
-        */
         HAL_GPIO_DeInit(
             GPIOF,
             GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10);
@@ -330,16 +298,15 @@ void HAL_ADC_MspDeInit(ADC_HandleTypeDef* adcHandle) {
 
         HAL_GPIO_DeInit(GPIOA, GPIO_PIN_3);
 
-        /* ADC3 DMA DeInit */
+        // ADC3 DMA DeInit
         HAL_DMA_DeInit(adcHandle->DMA_Handle);
 
-        /* ADC3 interrupt Deinit */
+        // ADC3 interrupt Deinit
 
-        /**
-         * Uncomment the line below to disable the "ADC_IRQn" interrupt
-         * Be aware, disabling shared interrupt may affect other IPs
-         */
-        /* HAL_NVIC_DisableIRQ(ADC_IRQn); */
+        // * Uncomment the line below to disable the "ADC_IRQn" interrupt
+        // * Be aware, disabling shared interrupt may affect other IPs
+
+        // HAL_NVIC_DisableIRQ(ADC_IRQn);
     }
 }
 }
